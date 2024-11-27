bf97baab7c61fec4aa2371879a5892a157a8bdf7
juppo <rbwilkes@yahoo.com>
2024-11-27 02:01:26 -0600

    add base character class

---
+++ classes/base.py
@@ -0,0 +1,11 @@
+class Base:
+    """Base class for all classes"""
+    def __init__(self, name, hp, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.name = name
+        self.hp = hp
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
