9a0ee5ef040520cf7febe0d8d8c93c7c82834468
juppo <rbwilkes@yahoo.com>
2024-12-01 12:59:34 -0600

    adding bow to package

---
+++ weapons/__init__.py
@@ -0,0 +1,7 @@
+from .normal import Normal
+from .martial import Martial
+from .axe import Axe
+from .sword import Sword
+from .polearm import Polearm
+from .flail import Flail
+from .bow import Bow
