1caa83a67fcc92f99f9b53b1b91a5cbdb142eca0
juppo <rbwilkes@yahoo.com>
2024-12-01 14:07:35 -0600

    add macro rolling algorithms to dice classes

--- dice/d10.py
+++ dice/d10.py
@@ -12,3 +12,14 @@ class D10:
     def roll(self):
         """Return a random value between 1 and number of sides."""
         return randint(1, self.num_sides)
+
+    def roll_two(self):
+        rolls = [self.roll() for _ in range(2)]
+        return rolls
+
+    def roll_three(self):
+        rolls = [self.roll() for _ in range(3)]
+        return rolls
+
+    def roll_percentage(self):
+        return randint(1, 100)
--- dice/d20.py
+++ dice/d20.py
@@ -12,3 +12,11 @@ class D20:
     def roll(self):
         """Return a random value between 1 and number of sides."""
         return randint(1, self.num_sides)
+
+    def roll_two(self):
+        rolls = [self.roll() for _ in range(2)]
+        return rolls
+
+    def roll_three(self):
+        rolls = [self.roll() for _ in range(3)]
+        return rolls
