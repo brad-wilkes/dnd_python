315f1b1eb8f64e4715714dc5b4280794cb8d04a8
juppo <rbwilkes@yahoo.com>
2024-12-01 14:05:19 -0600

    create rolling algorithms

--- dice/d6.py
+++ dice/d6.py
@@ -12,3 +12,19 @@ class D6:
     def roll(self):
         """Return a random value between 1 and number of sides."""
         return randint(1, self.num_sides)
+
+    def stat_roll(self):
+        rolls = [self.roll() for _ in range(5)]
+        rolls.sort()
+        return rolls[2:]
+
+    def stat_roll_sum(self):
+        return sum(self.stat_roll())
+
+    def roll_two(self):
+        rolls = [self.roll() for _ in range(2)]
+        return rolls
+
+    def roll_three(self):
+        rolls = [self.roll() for _ in range(3)]
+        return rolls
