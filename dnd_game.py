d608ec1f56d57dfd2db0d121f1c5250ce7900abd
juppo <rbwilkes@yahoo.com>
2024-12-01 12:59:53 -0600

    groundwork on base game

---
+++ dnd_game.py
@@ -0,0 +1,11 @@
+from classes.base import Base
+from dice import D20, D8, D6, D4, D12
+
+import pygame
+
+'''
+This is the main game file. It will handle the game loop, and the game logic.
+Use dice.py to create a dice object, and use classes/base.py to create a character object.
+The weapons are constituted in weapons package
+'''
+
